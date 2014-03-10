8bf23d55ffe4ab8277f5028cd234dc9816232194
Joseph Pawletko <jgpawletko@gmail.com>
2014-03-09 22:45:48 -0400

    snapshot

--- lib/nyudl/video/fcp/base.rb
+++ lib/nyudl/video/fcp/base.rb
@@ -1,8 +1,17 @@
+require 'nokogiri'
 module Nyudl
   module Video
     module Fcp
       class Base
-        def initialize
+        def initialize(f)
+          @file_path = f.dup
+
+          process_fcp_file
+        end
+
+        private
+        def process_fcp_file
+          f = File.open(@file_path)
         end
       end
     end
--- spec/nyudl/video/fcp/base_spec.rb
+++ spec/nyudl/video/fcp/base_spec.rb
@@ -1,9 +1,36 @@
 require 'spec_helper'
 
 describe Nyudl::Video::Fcp::Base do
-  context '.new' do
+  let(:fcp) { Nyudl::Video::Fcp::Base.new('test/template.xml') }
+  describe '.new' do
     it 'returns an object of the correct class' do
-      expect(Nyudl::Video::Fcp::Base.new()).to be_instance_of(Nyudl::Video::Fcp::Base)
+      expect(fcp).to be_instance_of(Nyudl::Video::Fcp::Base)
     end
+    it 'raises an exception if file cannot be found' do
+      expect {Nyudl::Video::Fcp::Base.new('asdfadfadsf')}.to raise_error(Errno::ENOENT)
+    end
+    it 'raises an exception if file is not readable'
   end
+# # raises and
+#   describe '#clips' do
+# #    let (:fcp) {
+#     context 'with a single clip' do
+# #      expect clip count to be correct
+#     end
+#     context 'with multiple clips
+#   end
+
+#   context 'with a single clip' do
+#     context '#clips' do
+#       it 'returns the correct clip array'
+#     end
+#     context '#digi_notes' do
+#       it 'returns the correct digitization notes'
+#     end
+#     context '#
+#     it 'returns the correct digitization notes'
+
+#   context 'with multiple clips'
+#   context 'with a clip missing an end marker'
+#   context 'with an unrecognized marker name'
 end
