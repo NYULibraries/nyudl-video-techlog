8b603823e1887551a194806dbf26044f489ff960
Joseph Pawletko <jgpawletko@gmail.com>
2014-03-11 09:15:23 -0400

    changed method names to "time_index"

--- lib/nyudl/video/techlog/time_code_29_97.rb
+++ lib/nyudl/video/techlog/time_code_29_97.rb
@@ -6,7 +6,7 @@ module Nyudl
         FRAMES_PER_SEC_29_97 = 29.97
 
         # This converts an arbitrary 29.97 frame count to a time in seconds (hh:mm:ss.msec)
-        def self.frames_to_time_in_sec(frame_count)
+        def self.frames_to_time_index(frame_count)
           seconds = frame_count / FRAMES_PER_SEC_29_97
 
           hh, rem = seconds.divmod(60 * 60)
@@ -18,10 +18,10 @@ module Nyudl
 
 
         # This converts an arbitrary time (hh:mm:ss.msec) to a 29.97 dropcode framecount
-        def self.time_in_sec_to_frames(timecode)
+        def self.time_index_to_frames(time_index)
 
           # parse incoming time
-          hh_s, mm_s, ss_s = timecode.split ':'
+          hh_s, mm_s, ss_s = time_index.split ':'
 
           hh = hh_s.to_i
           mm = mm_s.to_i
